0ba233d56784382d8e2a696e3fc64405785ba697
Darshan Pandya <andyadarshan811@gmail.com>
2024-03-19 22:34:19 +0530

    [Add] Footer and Used Daisy UI

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
+import { RiMovie2Fill } from "react-icons/ri";
 
 function Footer() {
   return (
-    <div className='footer flex items-center justify-center bg-black text-white font-semibold text-base tracking-widest'>
-      <footer>&copy; Copyright 2024 WatchWise</footer>
+   <footer className="footer footer-center p-10 bg-base-200 text-base-content">
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+      <a target='_blank' href='https://github.com/Darshan-Pandya10/WatchWise'><FaGithub size={26} /></a>
+      <a target='_blank' href='www.linkedin.com/in/darshanpandya811'><BsLinkedin size={26} /></a>
+      <a target='_blank' href='https://www.themoviedb.org/'><RiMovie2Fill size={26} /></a>
     </div>
+  </nav> 
+  <aside>
+    <p className='mb-8 tracking-wider'>Copyright © 2024 - All right reserved by <span className='text-[#6366f1] font-bold'>W</span>
+        atch
+        <span className='text-[#6366f1] font-bold'>W</span>
+        ise</p>
+  </aside>
+</footer>
   )
 }
 
--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -56,7 +56,7 @@ if(isError){
   )
 }
       console.log(data)
-      const {original_title : title , original_language : language , overview , poster_path : poster , release_date ,status , budget , revenue , homepage , genres , production_companies ,runtime } = data.data
+      const {original_title : title , original_language : language , overview , poster_path : poster , release_date ,status , budget , revenue , homepage , genres , production_companies , runtime , belongs_to_collection } = data.data
 
       const formattedBudget = budget.toLocaleString('en-US', {
         style: 'currency',
@@ -74,34 +74,40 @@ if(isError){
         if (minutes + ''.length < 2) {
           minutes = '0' + minutes; 
         }
-        return hours + ":" + minutes;
+        return hours + " : " + minutes;
      }
 
   return (
     <section className='details-page'>
+
       <h1 className='title text-2xl tracking-wider font-semibold bg-black text-white mx-auto my-2 px-8 py-4 w-fit sm:px-8 sm:py-4 md:w-fit text-center mb-8'>{title}</h1>
+
       <div className="container w-[90vw] sm:w-[90vw] md:w-[90vw] shadow-lg rounded-lg mb-12 md:mb-16 mx-auto p-8 pb-16 flex flex-col justify-center items-center md:flex md:flex-row md:justify-around md:items-center">
+
         <div className="poster relative w-fit mr-0 md:mr-8 flex items-center justify-center">
-          <span className='absolute text-white top-2 px-4 -left-6 tracking-wider bg-green-500 -rotate-45 rounded-bl-lg rounded-tr-lg shadow-2xl pb-1'>{status}</span>
-          <img src={`https://image.tmdb.org/t/p/w500/${poster}`} className='poster-image object-cover w-fit h-fit sm:object-cover sm:w-[20rem] sm:h-[30rem]' alt={`Poster Image of ${title} Movie`}/>
+          <span className='absolute text-white top-3 px-4 -left-6 tracking-wider bg-green-500 -rotate-45 rounded-bl-lg rounded-tr-lg shadow-2xl pb-1'>{status}</span>
+          <img src={`https://image.tmdb.org/t/p/w500/${poster}`} className='poster-image object-cover shadow-2xl w-fit h-fit sm:object-cover sm:w-[20rem] sm:h-[30rem]' alt={`Poster Image of ${title} Movie`}/>
         </div>
+
         <div className="details w-fit md:w-[30rem] mt-12 md:mt-0 h-full p-4">
           <p className='label'><span className='label-span'>Overview : </span> {overview}</p>
           <p className='label'><span className='label-span'>Original Language : </span> {language.toUpperCase()}</p>
-          <p className='label'><span className='label-span'>Genres : </span>{genres?.map((genre , index) => <p  key ={index} className='inline'>{genre.name} {index < genres.length - 1 ? ',' : ''} </p>)}</p>
+          <div className='label'><span className='label-span'>Genres : </span>{genres?.map((genre , index) => <p  key ={index} className='inline'>{genre.name} {index < genres.length - 1 ? ',' : ''} </p>)}</div>
+          {belongs_to_collection ? <p className='label'><span className='label-span'>Belongs to Collection : </span>{belongs_to_collection.name}</ p> : null }
+          
           <p className='label'><span className='label-span'>RunTime : </span> {NumToTime(runtime)} hours</p>
           <p className='label'><span className='label-span'>Release Date : </span> {release_date}</p>
           {/* <p className='label'><span className='label-span'>Status : </span> {status}</p> */}
           <p className='label'><span className='label-span'>Budget : </span> {formattedBudget}</p>
           <p className='label'><span className='label-span'>Revenue : </span> {formattedRevenue}</p>
-          <p className='label'><span className='label-span'>Production Company/Companies : </span>{production_companies?.map((company , index) => {
+          <div className='label'><span className='label-span'>Production Company/Companies : </span>{production_companies?.map((company , index) => {
             return <p key ={index} className='inline'> {company.name} {index < production_companies.length - 1 ? ',' : ''} </p>
-          })}</p>
-
-
-          <a className='px-4 py-2 my-2 font-semibold tracking-wide inline-block bg-[#6366F1] hover:bg-[#9193f7]' href={homepage}>Movie Homepage</a>
+          })}</div>
+          <a target='_blank' className='px-4 py-2 my-2 font-semibold tracking-wide inline-block bg-[#6366F1] hover:bg-[#9193f7]' href={homepage}>Movie Homepage</a>
         </div>
+
       </div>
+
     </section>
   )
 } 

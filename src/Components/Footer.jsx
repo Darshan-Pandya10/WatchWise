7c2ae2a826ad32d8eb22d5e76f3fd3d1cc77a661
Darshan Pandya <andyadarshan811@gmail.com>
2024-03-20 15:32:52 +0530

    Changes in Footer Design.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,14 +5,15 @@ import { RiMovie2Fill } from "react-icons/ri";
 
 function Footer() {
   return (
-   <footer className="footer bg-black p-10 w-screen">
-    <p className=' tracking-wider font-semibold text-white'>Copyright © 2024 - All right reserved by WatchWise.</p>
-    <div className="icons flex items-center justify-self-end">
-      <a target='_blank' href='https://github.com/Darshan-Pandya10/WatchWise' className='text-white hover:text-[#6366F1]'><FaGithub size={26} /></a>
-      <a target='_blank' href='www.linkedin.com/in/darshanpandya811' className='text-white hover:text-[#6366F1]'><BsLinkedin size={26} /></a>
-      <a target='_blank' href='https://www.themoviedb.org/' className='text-white hover:text-[#6366F1]'><RiMovie2Fill size={26} /></a>
-    </div>
+   <footer className="footer flex flex-col items-center justify-center bg-black w-screen">
+  <div className="icons flex items-center justify-center mt-2 -mb-8">
+    <a target='_blank' href='https://github.com/Darshan-Pandya10/WatchWise' className='m-2 text-white hover:text-[#6366F1]'><FaGithub size={26} /></a>
+    <a target='_blank' href='www.linkedin.com/in/darshanpandya811' className='m-2 text-white hover:text-[#6366F1]'><BsLinkedin size={26} /></a>
+    <a target='_blank' href='https://www.themoviedb.org/' className='m-2 text-white hover:text-[#6366F1]'><RiMovie2Fill size={26} /></a>
+  </div>
+  <p className='tracking-wider font-semibold text-white mb-4'>Copyright © 2024 - All rights reserved by WatchWise.</p>
 </footer>
+
   )
 }
 

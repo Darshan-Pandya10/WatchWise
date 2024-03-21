180ed6ed19ecf6f27b6dbddde7bd3368671bd2af
Darshan Pandya <andyadarshan811@gmail.com>
2024-03-21 19:54:12 +0530

    [Add] Browse Movie code  [WIP]

---
+++ src/Components/Form.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+function Form({ onSubmit , refetch}) {
+
+  const CurrentDate = new Date();
+  let CurrentYear = CurrentDate.getFullYear();
+  const [query, setQuery] = useState('');
+  const [includeAdult, setIncludeAdult] = useState(false);
+  const [language, setLanguage] = useState('en-US');
+  const [year, setYear] = useState(CurrentYear);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      query,
+      includeAdult,
+      language,
+      year
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex w-fit flex-col md:w-[36rem] md:flex-row md:justify-evenly md:items-center md:flex-wrap mx-auto my-16 px-4 py-8 bg-gray-100 rounded-lg">
+      <label className="block mb-2">
+        <span className="form-label">Movie Name*:</span>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          required
+          className='form-input'
+        />
+      </label>
+      <label className="block mb-2">
+        <span className="form-label">Adult:</span>
+        <select
+          value={includeAdult}
+          onChange={(e) => setIncludeAdult(e.target.value === 'true')}
+          className="form-input-short"
+        >
+          <option value="true">True</option>
+          <option value="false">False</option>
+        </select>
+      </label>
+      <label className="block mb-2">
+        <span className="form-label">Language:</span>
+        <input
+          type="text"
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+          className="form-input-short"
+        />
+      </label>
+      <label className="block mb-2">
+        <span className="form-label">Year*:</span>
+        <input
+          type="number"
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+          className="form-input-short"
+          max={CurrentYear}
+        />
+      </label>
+      <p className='text-gray-700 font-semibold tracking-wider'>(*) required input.</p>
+      <button type="submit" onClick={refetch} className="w-full font-semibold tracking-wider mt-4 py-2 bg-[#6366F1] text-white rounded-lg hover:bg-[#9193f7]">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
--- src/Pages/BrowseMovies.jsx
+++ src/Pages/BrowseMovies.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Form from '../Components/Form';
+import axios from 'axios';
+import MovieCard from '../Components/MovieCard';
 
 function BrowseMovies() {
+
+  const url = 'https://api.themoviedb.org/3/search/movie'
+  const [userInput , setUserInput] = useState({})
+  const queryFunc = async (options) => {
+    const response = await axios.request(url , options);
+    console.log(response)
+    return response;
+  };
+
+  const onSubmit = (searchInput) => { 
+    setUserInput(searchInput)   
+    const options = {
+      method: 'GET',
+      params: {
+        query: searchInput.query,
+        include_adult: searchInput.includeAdult.toString(),
+        language: searchInput.language,
+        primary_release_year: searchInput.year.toString,
+        page:'1',
+        region : 'usa',
+        year: searchInput.year
+      },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGFjMTgwMjM2MTIxZTRhYmVjZDdiMzcyMWI0Njg0MSIsInN1YiI6IjY1OWQwOGU1Zjg1OTU4MDFhODExOTY0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jl4vuS9kr0mczRAsBrD65L9yj0qZRMJ6OW_59d896zc'
+      }
+    };
+    queryFunc(options)
+  }
+
+ const {data , isLoading , isError , error, refetch} = useQuery({
+  queryKey : ['searchedMovie' , userInput.query],
+  queryFn: queryFunc,
+  enabled:false
+ })
+
+console.log(data)
+
+  //Loading Screen
+
+if(isLoading){
   return (
-    <div className='browse-movies min-h-screen pt-32 px-4'>
-      <h1>This is a BrowseMovies</h1>
-    </div>
+  <section className='browse-movies pt-32'>
+   <div className="loader"></div>
+  </section>
   )
 }
 
-export default BrowseMovies
+//404 Error Image
+
+if(error?.response?.status === 404){
+  return(
+    <section className='browse-movies flex items-center justify-center'>
+      <img src='../src/assets/404-error.svg' alt='404 error image' className='object-cover w-fit h-fit' />
+    </section>
+  )
+}
+
+//For anyother error excluding 404
+
+if(isError){
+  return (
+  <section className='browse-movies pt-52'>
+   <h1 className='font-bold text-xl tracking-wider'>Error : {error.message}</h1>
+  </section>
+  )
+}
+
+  return (
+
+    <div className='browse-movies min-h-screen pt-52 md:pt-20 px-4'>
+      <Form onSubmit={onSubmit} refetch={refetch}/>
+      <div className='movie-slide flex justify-start items-start overflow-x-scroll overflow-y-hidden'>
+        {/* { data?.data?.results?.length >=1 && 
+        data?.data?.results.map((movie) => {
+        const id = uuidv4()
+        return <MovieCard movie={movie} key={id}/>
+      })} */}
+      </div>
+    </div>
+  );
+}
+
+export default BrowseMovies;

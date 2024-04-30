fda6e9f45f7a74e6a98e673a9fc1567bf41e3322
Darshan Pandya <andyadarshan811@gmail.com>
2024-04-30 14:43:19 +0530

    More Styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
 
 
   return (  
-    <div className='header fixed h-fit z-50 backdrop-blur-lg w-screen shadow-lg flex flex-col justify-around items-start p-4 sm:flex sm:flex-row sm:justify-between sm:items-center'>
+    <div className='header fixed h-fit z-50 backdrop-blur-2xl w-screen shadow-lg flex flex-col justify-around items-start p-4 sm:flex sm:flex-row sm:justify-between sm:items-center'>
       <Link to='/' className='flex'>
       <h1 className='font-semibold text-xl md:text-2xl'>
         <span className='text-[#6366f1] font-bold'>W</span>
@@ -40,16 +40,8 @@ function Header() {
       <button onClick={handleNavbar} className='absolute top-4 right-4'> {isNavbarVisible ? <RxCross1 className='hover:text-[#6366F1]' size={26} /> : <RxHamburgerMenu className='hover:text-[#6366F1]'  size={26} />}</button>
       </div>
         <nav className={`mt-3 sm:mt-0 ${
-          isNavbarVisible ? '' : 'hidden'
-        } flex flex-col items-left justify-center justify-self-center place-items-center
-
-
-         w-[90vw] mx-auto min-h-screen sm:flex-row sm:justify-end sm:items-end  sm:min-h-fit sm:ml-4  
-        
-        font-semibold text-[1rem]`}>
-
-
-
+          isNavbarVisible ? '' : 'hidden'} flex flex-col items-left justify-center justify-self-center place-items-center
+          w-[90vw] mx-auto min-h-screen sm:flex sm:flex-row sm:justify-end sm:items-end  sm:min-h-fit sm:ml-4 font-semibold text-[1rem]`}>
 
 
           <NavLink onClick={closeNavbar} className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base mb-1 sm:mr-3 sm:mb-0' to='/'> 

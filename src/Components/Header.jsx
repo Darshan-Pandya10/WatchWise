1df895c99a4b42e602b8d26bbe8bb2cbddf2a278
Darshan Pandya <andyadarshan811@gmail.com>
2024-04-30 13:56:56 +0530

    [Add] Navbar Icons for small screens and add closing functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import { NavLink , Link } from 'react-router-dom'
-import { RxHamburgerMenu } from "react-icons/rx";
-import { RxCross1 } from "react-icons/rx";
 import '../App.css'
 import TabSvg from '../assets/TabSvg.svg'
+
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
+import { HiOutlineHome } from "react-icons/hi";
+import { FiSearch } from "react-icons/fi";
+import { MdOutlineBookmarkAdd } from "react-icons/md";
+import { CgDetailsMore } from "react-icons/cg";
 
 function Header() {
   const [isNavbarVisible, setIsNavbarVisible] = useState(false);
@@ -11,6 +16,11 @@ function Header() {
   const handleNavbar = () => {
     setIsNavbarVisible(!isNavbarVisible);
   }
+
+    const closeNavbar = () => {
+    setIsNavbarVisible(false);
+  };
+
 
   return (  
     <div className='header fixed h-fit z-50 backdrop-blur-lg w-screen shadow-lg flex flex-col justify-around items-start p-4 sm:flex sm:flex-row sm:justify-between sm:items-center'>
@@ -42,10 +52,15 @@ function Header() {
 
 
 
-          <NavLink className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base mb-1 sm:mr-3 sm:mb-0' to='/'>Home</NavLink>
-          <NavLink className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='browse-movies'>BrowseMovies</NavLink>
-          <NavLink className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='watchlist'>WatchList</NavLink>
-          <NavLink className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='about'>About</NavLink>
+          <NavLink onClick={closeNavbar} className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base mb-1 sm:mr-3 sm:mb-0' to='/'> 
+          <HiOutlineHome  className='inline-block sm:hidden mr-1' /> Home
+          </NavLink>
+          <NavLink onClick={closeNavbar} className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='browse-movies'> <FiSearch className='inline-block sm:hidden mr-1'  /> BrowseMovies
+          </NavLink>
+          <NavLink onClick={closeNavbar} className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='watchlist'> <MdOutlineBookmarkAdd className='inline-block sm:hidden mr-1'  /> WatchList
+          </NavLink>
+          <NavLink onClick={closeNavbar} className='navlink text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='about'> <CgDetailsMore className='inline-block sm:hidden mr-1'  /> About
+          </NavLink>
         </nav>
     </div>
 
